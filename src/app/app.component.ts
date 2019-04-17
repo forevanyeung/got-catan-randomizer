2e342452b1368f04e8c26ceaee530711c3bf6b9b
Evan Yeung <evan@forevanyeung.com>
2019-04-17 11:00:02 -0400

    Changed map array structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,13 +32,19 @@ export class AppComponent {
   ]
 
   combinedEx = [
-    [5,"ore"],[6,"wool"],[5,"brick"],[8,"wheat"],
-    [6,"ore"],[4,"brick"],[9,"wheat"],[10,"wool"],[11,"lumber"],
-    [3,"wool"],[9,"lumber"],[11,"wool"],[3,"ore"],[8,"lumber"],[4,"ore"],
-    [10,"wheat"],[12,"lumber"],[3,"brick"],[10,"wheat"],[4,"brick"],[2,"ore"]
+    [[5,"ore"],[6,"wool"],[5,"brick"],[8,"grain"]],
+    [[6,"ore"],[4,"brick"],[9,"grain"],[10,"wool"],[11,"lumber"]],
+    [[3,"wool"],[9,"lumber"],[11,"wool"],[3,"ore"],[8,"lumber"],[4,"ore"]],
+    [[10,"grain"],[12,"lumber"],[3,"brick"],[10,"grain"],[4,"brick"],[2,"ore"]]
   ]
 
   array(n: number): any[] {
     return Array(n);
+  }
+
+  notafunction() {
+    let temp = this.combinedEx;
+
+
   }
 }
