669c2b77024739b531610ddb8a60d25975a576c7
Evan Yeung <evan@forevanyeung.com>
2019-04-17 08:38:23 -0400

    Updated CSS. Started dynamically generating hex grid.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  /* 4-player */
+  rows = [
+    4,
+    5,
+    6,
+    6
+  ]
+  tokens = [
+    2,3,3,3,4,4,4,5,5,6,6,8,8,9,9,10,10,10,11,11,12
+  ]
+  hexes = [
+    "grain","grain","grain","grain",
+    "brick","brick","brick","brick",
+    "lumber","lumber","lumber","lumber",
+    "wool","wool","wool","wool",
+    "ore","ore","ore","ore","ore"
+  ]
+  ports = [
+    "grain",
+    "brick",
+    "lumber",
+    "wool",
+    "ore"
+  ]
+
+  combinedEx = [
+    [5,"ore"],[6,"wool"],[5,"brick"],[8,"wheat"],
+    [6,"ore"],[4,"brick"],[9,"wheat"],[10,"wool"],[11,"lumber"],
+    [3,"wool"],[9,"lumber"],[11,"wool"],[3,"ore"],[8,"lumber"],[4,"ore"],
+    [10,"wheat"],[12,"lumber"],[3,"brick"],[10,"wheat"],[4,"brick"],[2,"ore"]
+  ]
+
+  array(n: number): any[] {
+    return Array(n);
+  }
 }
